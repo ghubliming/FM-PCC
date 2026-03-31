e32fb41fce6616b85c5e829163cd9760da17f98f
ghubliming <139686052+ghubliming@users.noreply.github.com>
2026-03-31 10:30:44 +0000

    refactor: rename UNet1DTemporalCondModel to Flow_matcher_U_Net_v2 to resolve dynamic import errors

--- flow_matcher_unet_v2/models/__init__.py
+++ flow_matcher_unet_v2/models/__init__.py
@@ -1,2 +1,2 @@
-from .unet1d_temporal_cond import UNet1DTemporalCondModel, TemporalValue, MLPnet
+from .unet1d_temporal_cond import Flow_matcher_U_Net_v2, TemporalValue, MLPnet
 from .diffusion import GaussianDiffusion
